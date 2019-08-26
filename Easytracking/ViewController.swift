69da22952bb215b071ee4b22a78f2a88171d9738
lucasfeitosatech <lucasfeitosa.tech@gmail.com>
2019-08-26 09:39:47 -0300

    Initial Commit

---
+++ Easytracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Easytracking
+//
+//  Created by Lucas Freitas on 26/08/19.
+//  Copyright © 2019 First Decision. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
